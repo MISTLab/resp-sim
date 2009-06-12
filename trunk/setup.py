1bfc8fecb1f926cfcd213b77a26d3d2d7186e9ea
fossati.l <fossati.l@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2009-06-12 09:26:15 +0000

    Update the setup helper

--- trunk/setup.py
+++ trunk/setup.py
@@ -114,76 +114,60 @@ if __name__ == '__main__':
     # Configuring the project for compilation
     try:
         print "You will now be asked to enter some information on your environment\n"
-        systemc_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'SystemC library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+        systemc_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'SystemC 2.2.0 library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
         while not os.path.exists(systemc_dir):
-            systemc_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'SystemC library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
-        tlm_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'TLM library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+            systemc_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'SystemC 2.2.0 library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+        tlm_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'TLM 2.0 library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
         while not os.path.exists(tlm_dir):
-            tlm_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'TLM library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
-#MATLAB
-        matlab_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'MATLAB ' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))      
+            tlm_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'TLM 2.0 library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+        trap_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'TRAP library' + colors.colorMap['none'] + ' (just press RETURN for their automatic detection): ')).replace('\n', ''))
+        while not os.path.exists(trap_dir) and trap_dir != '':
+            trap_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'TRAP library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+        bfd_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'binutils (bfd, iberty) library' + colors.colorMap['none'] + ' (just press RETURN for their automatic detection): ')).replace('\n', ''))
+        while not os.path.exists(bfd_dir) and bfd_dir != '':
+            bfd_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'binutils (bfd, iberty) library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
         boost_head_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'boost library headers' + colors.colorMap['none'] + ' (just press RETURN for their automatic detection): ')).replace('\n', ''))
         while not os.path.exists(boost_head_dir) and boost_head_dir != '':
             boost_head_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'boost library headers' + colors.colorMap['none'] + ': ')).replace('\n', ''))
         boost_lib_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'boost library libraries' + colors.colorMap['none'] + ' (just press RETURN for their automatic detection): ')).replace('\n', ''))
         while not os.path.exists(boost_lib_dir) and boost_lib_dir != '':
             boost_head_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'boost library libraries' + colors.colorMap['none'] + ': ')).replace('\n', ''))
-        debug = (raw_input('Do you want to have the possibility of ' + colors.colorMap['bright_green'] + 'debugging' + colors.colorMap['none'] + ' simulated programs (it slows down execution) [y/N]: ')).replace('\n', '')
-        profile = (raw_input('Do you want to have the possibility of ' + colors.colorMap['bright_green'] + 'profiling' + colors.colorMap['none'] + ' simulated programs (it slows down execution) [y/N]: ')).replace('\n', '')
-        trace = (raw_input('Do you want to have the possibility of ' + colors.colorMap['bright_green'] + 'producing debug traces' + colors.colorMap['none'] + ' of the instructions executed by the processors (it slows down execution) [y/N]: ')).replace('\n', '')
-        os_emu = (raw_input('Do you want to enable ' + colors.colorMap['bright_green'] + 'Operating System Emulation' + colors.colorMap['none'] + ' (if you only need to run a real OS, such as eCos, this option will only slow you down) [Y/n]: ')).replace('\n', '')
-        processors = (raw_input('Specify the ' + colors.colorMap['bright_green'] + 'processors' + colors.colorMap['none'] + ' that you want to use (separate the names with a space, nothing to enable all of them) [arm7 arm7ca leon2 ppc405 powerpc mips1]: ')).replace('\n', '').lower()
-        arm_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' arm cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
-        sparc_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' sparc cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
-        mips_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' mips cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
-        ppc_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' powerpc cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
-        debug_level = (raw_input('Specify the ' + colors.colorMap['bright_green'] + 'compilation flags' + colors.colorMap['none'] + ' [debug/release/OPTIMIZED]: ')).replace('\n', '')
-        eCos_path = os.path.expanduser((raw_input('Specify the ' + colors.colorMap['bright_green'] + 'eCos source folder' + colors.colorMap['none'] + ' (just press RETURN if you do not need eCos): ')).replace('\n', ''))
-        if eCos_path != '':
-            wxGTK_path = os.path.expanduser((raw_input('Specify the ' + colors.colorMap['bright_green'] + 'wxGTK-2.4.0' + colors.colorMap['none'] + ' (just press RETURN if you do not need the graphical eCos support tools): ')).replace('\n', ''))
-            eCos_dest = os.path.expanduser((raw_input('Specify the ' + colors.colorMap['bright_green'] + 'destination for the eCos support binaries' + colors.colorMap['none'] + ' [~/ecos-bin]: ')).replace('\n', ''))
-        tests = (raw_input('Do you want to ' + colors.colorMap['bright_green'] + 'run the tests' + colors.colorMap['none'] + ' at the end of the compilation (it will take a lot of time) [Y/n]: ')).replace('\n', '')
+        bfd_dir = os.path.expanduser((raw_input('Enter the location of the ' + colors.colorMap['bright_green'] + 'binutils (bfd, iberty) library' + colors.colorMap['none'] + ' (just press RETURN for their automatic detection): ')).replace('\n', ''))
+        while not os.path.exists(bfd_dir) and bfd_dir != '':
+            bfd_dir = os.path.expanduser((raw_input('Please specify an existing path for the ' + colors.colorMap['bright_green'] + 'binutils (bfd, iberty) library' + colors.colorMap['none'] + ': ')).replace('\n', ''))
+        tools = (raw_input('Do you want to have the possibility of ' + colors.colorMap['bright_green'] + 'using tools' + colors.colorMap['none'] + ' (os-emulator, debugger, etc -- it slows down execution) [Y/n]: ')).replace('\n', '')
+        #processors = (raw_input('Specify the ' + colors.colorMap['bright_green'] + 'processors' + colors.colorMap['none'] + ' that you want to use (separate the names with a space, nothing to enable all of them) [arm7 arm7ca leon2 ppc405 powerpc mips1]: ')).replace('\n', '').lower()
+        #arm_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' arm cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
+        #sparc_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' sparc cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
+        #mips_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' mips cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
+        #ppc_cross_path = os.path.expanduser((raw_input('Specify the path of the' + colors.colorMap['bright_green'] + ' powerpc cross-compiler' + colors.colorMap['none'] + ' (just press RETURN if you do not need it): ')).replace('\n', ''))
 
-        options = "--with-systemc=" + systemc_dir + " --with-tlm=" + tlm_dir 
-        if matlab_dir != "":
-            options += " --with-matlab=" + matlab_dir
+        options = "--with-systemc=" + systemc_dir + " --with-tlm=" + tlm_dir
+        if trap_dir != "":
+            options += " --with-trap=" + trap_dir
+        if bfd_dir != "":
+            options += " --with-bfd=" + bfd_dir
         if boost_head_dir != "":
             options += " --boost-includes=" + boost_head_dir
         if boost_lib_dir != "":
             options += " --boost-libs=" + boost_lib_dir
-        if debug.lower() == "y":
-            options += " -D"
-        if profile.lower() == "y":
-            options += " -P"
-        if trace.lower() == "y":
+        if tools.lower() == "n":
             options += " -T"
-        if os_emu.lower() == "n":
-            options += " -S"
-        if processors != "":
-            options += " --enable-processor=\"" + processors + "\""
-        if arm_cross_path != "":
-            options += " --arm-compiler=" + arm_cross_path
-        if sparc_cross_path != "":
-            options += " --sparc-compiler=" + sparc_cross_path
-        if mips_cross_path != "":
-            options += " --mips-compiler=" + mips_cross_path
-        if ppc_cross_path != "":
-            options += " --ppc-compiler=" + ppc_cross_path
-        if debug_level != "":
-            options += " --debug " + debug_level
-        if eCos_path != "":
-            options += " --ecos-path=" + eCos_path
-            if wxGTK_path != "":
-                options += " --wxgtk-path=" + wxGTK_path
-            else:
-                print colors.colorMap['bright_green'] + 'Warning:' + colors.colorMap['none'] + ' the wxGTK path was specified, but eCos is not going to be built'
-            if eCos_dest != "":
-                options += " --ecos-bin-dest=" + eCos_dest
+        #if processors != "":
+            #options += " --enable-processor=\"" + processors + "\""
+        #if arm_cross_path != "":
+            #options += " --arm-compiler=" + arm_cross_path
+        #if sparc_cross_path != "":
+            #options += " --sparc-compiler=" + sparc_cross_path
+        #if mips_cross_path != "":
+            #options += " --mips-compiler=" + mips_cross_path
+        #if ppc_cross_path != "":
+            #options += " --ppc-compiler=" + ppc_cross_path
 
         raw_input('\nPress RETURN to ' + colors.colorMap['bright_brown'] + 'start the project configuration' + colors.colorMap['none'])
         print ''
 
-        conf_result = os.system("./waf configure " + options + " --nocache")
+        conf_result = os.system("./waf configure " + options)
 
         if conf_result != 0:
             print '\n' + colors.colorMap['bright_red'] + 'An error occurred during ReSP configuration process' + colors.colorMap['none'] + '\n'
@@ -199,10 +183,7 @@ if __name__ == '__main__':
         print '\n\n' + colors.colorMap['bright_red'] + 'Installation process aborted' + colors.colorMap['none'] + '\n'
         sys.exit(0)
 
-    if tests.lower() == "n":
-        conf_result = os.system("./waf")
-    else:
-        conf_result = os.system("./waf --execute-tests")
+    conf_result = os.system("./waf")
 
     if conf_result != 0:
         print '\n' + colors.colorMap['bright_red'] + 'An error occurred during ReSP compilation process' + colors.colorMap['none']
@@ -211,4 +192,3 @@ if __name__ == '__main__':
 
     print '\nCompilation succesfully completed'
     print 'In future, if you want to compile modifications you make to the code just run the command \'./waf\' at the command prompt'
-    print '\n' + colors.colorMap['bright_red'] + 'Type ' + colors.colorMap['bright_brown'] + '\'./startSim.sh -a architectures/test_arch.py\'' + colors.colorMap['bright_red'] + ' at the command prompt to quickly test ReSP installation' + colors.colorMap['none'] + '\n'
