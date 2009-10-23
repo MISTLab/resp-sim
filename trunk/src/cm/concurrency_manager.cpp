89e39b6c304236547c2a5ce2e077f2c203b75517
fossati.l <fossati.l@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2009-10-23 06:22:25 +0000

    Continued with the implementation of the concurrency manager

--- trunk/src/cm/concurrency_manager.cpp
+++ trunk/src/cm/concurrency_manager.cpp
@@ -71,10 +71,31 @@ unsigned int resp::ConcurrencyManager::tlsSize = 0;
 unsigned char * resp::ConcurrencyManager::tlsData = NULL;
 
 resp::ConcurrencyManager::ConcurrencyManager(){
+    //Constructor: there is not much to do
+    //a part from se-setting the reentracy
+    //variables
+    this->mallocMutex = -1;
+    this->sfpMutex = -1;
+    this->sinitMutex = -1;
+    this->fpMutex = -1;
+    this->maxProcId = 0;
 }
 
 ///Resets the CM to its original status as after the construction
 void resp::ConcurrencyManager::reset(){
+    //Resetting static variables
+    resp::ConcurrencyManager::busyWaitLoop = true;
+    resp::ConcurrencyManager::threadStackSize = 1024*20;
+    resp::ConcurrencyManager::defThreadInfo.clear();
+    resp::ConcurrencyManager::interruptServiceRoutines.clear();
+    resp::ConcurrencyManager::tlsSize = 0;
+    resp::ConcurrencyManager::tlsData = NULL;
+    //Resetting instance variables
+    this->mallocMutex = -1;
+    this->sfpMutex = -1;
+    this->sinitMutex = -1;
+    this->fpMutex = -1;
+    this->maxProcId = 0;
 }
 
 ///*******************************************************************
@@ -83,6 +104,10 @@ void resp::ConcurrencyManager::reset(){
 ///Initializes the mutexes used to guarantee mutual exclusion access to
 ///reentrant routines
 void resp::ConcurrencyManager::initReentrantEmulation(){
+    this->mallocMutex = this->createMutex();
+    this->sfpMutex = this->createMutex();
+    this->sinitMutex = this->createMutex();
+    this->fpMutex = this->createMutex();
 }
 
 ///*******************************************************************
@@ -90,18 +115,33 @@ void resp::ConcurrencyManager::initReentrantEmulation(){
 /// execution
 ///*******************************************************************
 void resp::ConcurrencyManager::printThreadTraces(){
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
 }
 std::vector<std::string> resp::ConcurrencyManager::getThreadTraces(){
+    std::vector<std::string> traces;
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
+    return traces;
 }
 std::map<int, double> resp::ConcurrencyManager::getAverageLoads(){
+    std::map<int, double> avgLoad;
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
+    return avgLoad;
 }
 void resp::ConcurrencyManager::printIdleTraces(){
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
 }
 std::vector<std::string> resp::ConcurrencyManager::getIdleTraces(){
+    std::vector<std::string> idleTrace;
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
+    return idleTrace;
 }
 int resp::ConcurrencyManager::getDeadlineMisses(){
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
+    return 0;
 }
 int resp::ConcurrencyManager::getDeadlineOk(){
+    THROW_EXCEPTION(__PRETTY_FUNCTION__ << " not yet implemented");
+    return 0;
 }
 
 ///*******************************************************************
@@ -110,7 +150,117 @@ int resp::ConcurrencyManager::getDeadlineOk(){
 /// the emulated concurrency related primitives
 ///*******************************************************************
 ///*********** Thread related routines *******************
-int resp::ConcurrencyManager::createThread(unsigned int threadFun,  unsigned int args, int attr){
+int resp::ConcurrencyManager::createThread(unsigned int threadFun, unsigned int args, int attr){
+    //I have to create the thread handle and add it to the list of
+    //existing threads; in case there is an available processor,
+    //I also start the execution of the thread on it; note that all the
+    //data structures touched here are also used during scheduling,
+    //so synchronization must be enforced with respect to those structures
+
+    this->schedLock.lock();
+    //First of all lets determine the stack size
+/*    unsigned int curStackBase = 0;
+    unsigned int curStackSize = 0;
+    if(existingAttr.find(attr) == existingAttr.end()){
+        curStackSize = defaultStackSize;
+    }
+    else{
+        curStackSize = existingAttr[attr]->stackSize;
+    }
+    //I have to determine if there is space for
+    //my stack among two already existing ones
+    //or if I have to position myself at the end of
+    //the stack
+    std::map<unsigned int, unsigned int>::iterator stacksIter, stacksNext, stacksEnd;
+    for(stacksIter = stacks.begin(), stacksNext = stacksIter, stacksNext++, stacksEnd = stacks.end();
+                                                                        stacksNext != stacksEnd; stacksIter++, stacksNext++){
+        if((stacksIter->first - stacksIter->second - stacksNext->first) > curStackSize){
+            curStackBase = stacksIter->first - stacksIter->second;
+            stacks[curStackBase] = curStackSize;
+            break;
+        }
+    }
+    if(curStackBase == 0){
+        //I haven't found any suitable stack
+        curStackBase = stacksIter->first - stacksIter->second;
+        stacks[curStackBase] = curStackSize;
+    }
+    if(curStackBase < archc::ac_heap_ptr)
+        THROW_EXCEPTION("Unable to allocate " << curStackSize << " bytes for the stack of thread " << existingThreads.size() << ": no more space in memory");
+    ThreadEmu *th = NULL;
+    AttributeEmu *curAttr = NULL;
+    std::string routineName = bfdFE->symbolAt((unsigned long)threadFun).front();
+    if(existingAttr.find(attr) == existingAttr.end()){
+        if(defThreadInfo.find(routineName) == defThreadInfo.end()){
+            th = new ThreadEmu(existingThreads.size(), threadFun, args, curStackBase, defaultTLSSize, &defaultAttr);
+        }
+        else{
+            curAttr = existingAttr[createThreadAttr()];
+            curAttr->preemptive = defThreadInfo[routineName].preemptive;
+            curAttr->schedPolicy = defThreadInfo[routineName].schedPolicy;
+            curAttr->priority = defThreadInfo[routineName].priority;
+            curAttr->deadline = defThreadInfo[routineName].deadline;
+            th = new ThreadEmu(existingThreads.size(), threadFun, args, curStackBase, defaultTLSSize, curAttr);
+        }
+    }
+    else{
+        curAttr = existingAttr[attr];
+        if(defThreadInfo.find(routineName) != defThreadInfo.end()){
+            curAttr->preemptive = defThreadInfo[routineName].preemptive;
+            curAttr->schedPolicy = defThreadInfo[routineName].schedPolicy;
+            curAttr->priority = defThreadInfo[routineName].priority;
+            curAttr->deadline = defThreadInfo[routineName].deadline;
+        }
+        th = new ThreadEmu(existingThreads.size(), threadFun, args, curStackBase, defaultTLSSize, curAttr);
+    }
+    existingThreads[existingThreads.size()] = th;
+
+    //I schedule the thread if I can find a free processor
+    #ifndef NDEBUG
+    std::cerr << "Trying to schedule thread " << existingThreads.size() - 1 << " on a free processor" << std::endl;
+    #endif
+    if(!scheduleFreeProcessor(existingThreads[existingThreads.size() - 1])){
+        //Ok, I haven't found any free processor; I try to determine
+        //if there is a processor running a lower priority thread and, in case,
+        //I replace it
+        #ifndef NDEBUG
+        std::cerr << "Trying to schedule thread " << existingThreads.size() - 1 << " through preemption" << std::endl;
+        #endif
+        if(!preemptLowerPrio(existingThreads[existingThreads.size() - 1])){
+            #ifndef NDEBUG
+            std::cerr << "Unable to preempt any thread, adding " << existingThreads.size() - 1 << " to the ready queue" << std::endl;
+            #endif
+            //Ok, nothing else I can do: lets add the thread to the correct
+            //ready queue
+            int curPrio = 0;
+            if(curAttr != NULL){
+                if(curAttr->schedPolicy == resp::SYSC_SCHED_EDF)
+                    curPrio = resp::SYSC_PRIO_MAX + 1;
+                else
+                    curPrio = curAttr->priority;
+            }
+            #ifndef NDEBUG
+            if(curPrio < resp::SYSC_PRIO_MIN || curPrio > resp::SYSC_PRIO_MAX + 1){
+                THROW_EXCEPTION("Non valid priority value " << curPrio);
+            }
+            #endif
+            readyQueue[curPrio].push_back(existingThreads[existingThreads.size() - 1]);
+            if(curPrio == resp::SYSC_PRIO_MAX + 1)
+                sort(readyQueue[curPrio].begin(), readyQueue[curPrio].end(), deadlineSort);
+        }
+    }
+
+    #ifndef NDEBUG
+    std::cerr << "Created thread Id = " << existingThreads.size() - 1 << \
+                " stack size = " << curStackSize/1024 << "KB" << " stack base " << \
+                std::showbase << std::hex << curStackBase << std::dec << " status " << \
+                th->status << " address " << std::hex << std::showbase << threadFun <<  std::endl;
+    #endif
+
+    schedulingLockBusy = false;
+    schedulingEvent.notify();
+
+    return (existingThreads.size() - 1);*/
 }
 void resp::ConcurrencyManager::exitThread(unsigned int procId, unsigned int retVal){
 }
@@ -193,7 +343,7 @@ void resp::ConcurrencyManager::execCleanupHandlerTop(unsigned int procId){
 ///Destroys a previously allocated mutex, exception if the mutex does not exist
 void resp::ConcurrencyManager::destroyMutex(unsigned int procId, int mutex){
 }
-int resp::ConcurrencyManager::createMutex(unsigned int procId){
+int resp::ConcurrencyManager::createMutex(){
 }
 ///Locks the mutex, deschedules the thread if the mutex is busy; in case
 ///nonbl == true, the function behaves as a trylock
