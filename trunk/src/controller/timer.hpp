3c5c70037ea2338eaf0269da5780d33e4c664c40
antoniorosmiele <antoniorosmiele@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2010-05-04 14:36:10 +0000

    substituted boost.timer with a custom one. boost.timer overflows after approximatelly 30 minutes...

--- trunk/src/controller/timer.hpp
+++ trunk/src/controller/timer.hpp
@@ -2,14 +2,29 @@
 #define TIMER_HPP
 
 #include <ctime>
+#include <sys/time.h>
 
 class timer
 {
  public:
-    timer() { _start_time = std::time(NULL); } // postcondition: elapsed()==0
-    void   restart() { _start_time = std::time(NULL); } // post: elapsed()==0
-    long int elapsed() const                  // return elapsed time in seconds
-    { return  std::time(NULL) - _start_time; }
+    timer() { // postcondition: elapsed()==0
+      struct timeval tv;
+      gettimeofday(&tv, NULL); 
+      _start_time=tv.tv_sec;
+    } 
+    void   restart() { // post: elapsed()==0
+      struct timeval tv;
+      gettimeofday(&tv, NULL); 
+      _start_time=tv.tv_sec;
+    }
+    
+    long int elapsed() const { // return elapsed time in seconds
+      struct timeval tv;
+      std::time_t now;
+      gettimeofday(&tv, NULL); 
+      now=tv.tv_sec;
+      return  now - _start_time; 
+    }
 
  private:
   std::time_t _start_time;
