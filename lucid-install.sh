7915bdd1b0b6f20c1e4c7e4a794fcd29a763f41a
arlix85 <arlix85@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2010-06-01 10:16:25 +0000

    Updated and unified automatic installers for Ubuntu Lucid and Karmic

--- lucid-install.sh
+++ lucid-install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 ################################################################################
-#
-#
-#
-#
 #              ___           ___           ___           ___
 #             /  /\         /  /\         /  /\         /  /\
 #            /  /::\       /  /:/_       /  /:/_       /  /::\
@@ -17,17 +13,8 @@
 #            \  \:\        \  \::/        /__/:/       \  \:\
 #             \__\/         \__\/         \__\/         \__\/
 #
-#
-#
-#
-#
-#
 #         v0.5 - Politecnico di Milano, European Space Agency
 #            This tool is distributed under the GPL License
-#
-#
-#
-#
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -44,51 +31,48 @@
 #  Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
 #
-#
-#
-#
 ################################################################################
 
-# Install script for Ubuntu 10.04 Lucid Lynx
+# Install script for Ubuntu 10.04 Lucid Lynx and Ubuntu 9.10 Karmic Koala
 
 # Prerequisite packages
-#   GCC 4.2
-sudo apt-get -y install libsigc++-2.0-dev subversion binutils-dev mpi-default-dev g++ g++-4.4 g++-4.1 gccxml libgfortran3 libibverbs-dev libibverbs1 libicu-dev libnuma1 libopenmpi-dev libopenmpi1.3 libstdc++6-4.4-dev openmpi-common python-dev python2.6-dev
+#   GCC 4.1
 #   SVN
 #   gccxml
 #   binutils
-#   doxygen (sorry)
+sudo apt-get -y install libsigc++-2.0-dev subversion binutils-dev mpi-default-dev g++ g++-4.4 g++-4.1 gccxml libgfortran3 libibverbs-dev libibverbs1 libicu-dev libnuma1 libopenmpi-dev libopenmpi1.3 libstdc++6-4.4-dev openmpi-common python-dev python2.6-dev
 
 mkdir External_tools
 cd External_tools
 
-#boost
+# boost
+#     download
 wget home.dei.polimi.it/miele/boost_1_42_0_deb.tar.bz2
 tar xvjf boost_1_42_0_deb.tar.bz2
 cd boost_1_42_0_deb
-sudo dpkg -i *.deb
+sudo dpkg -i libicu*.deb
+sudo dpkg -i libboost*.deb
 cd ..
 
-
 # Pyplusplus
-#   pygccxml
-#       download/install
+#     pygccxml
+#         download/install
 svn co https://pygccxml.svn.sourceforge.net/svnroot/pygccxml/pygccxml_dev pygccxml
 cd pygccxml
 sudo python setup.py install
 cd ..
-#   py++
-#       download/install
+#     py++
+#         download/install
 svn co https://pygccxml.svn.sourceforge.net/svnroot/pygccxml/pyplusplus_dev pyplusplus
 cd pyplusplus
 sudo python setup.py install
 cd ..
 
 # libMOMH
-#   download
+#     download
 wget http://www.jumpjoe.com/sysc/libmomh-1.91.3.tar.bz2
 tar xvjf libmomh-1.91.3.tar.bz2
-#   configure/install
+#     configure/install
 cd libmomh-1.91.3
 ./configure
 make -j 2
@@ -99,6 +83,7 @@ cd ..
 wget http://www.jumpjoe.com/sysc/systemc-2.2.0_gcc4.tar.bz2
 tar xvjf systemc-2.2.0_gcc4.tar.bz2
 cd systemc-2.2.0
+sudo rm -r lib-linux
 ./configure
 make -j 2
 sudo make install
@@ -109,27 +94,30 @@ wget http://www.jumpjoe.com/sysc/TLM2.tar.bz2
 tar xvjf TLM2.tar.bz2
 
 # TRAP
-#   download
+#     download
 svn checkout http://trap-gen.googlecode.com/svn/trunk/ trap-gen
-#   configure/install
+#     configure/install
 cd trap-gen
 ./waf configure --with-systemc=../systemc-2.2.0
 ./waf
 sudo ./waf install
 cd ..
 
-#download crosscompilers
-wget home.dei.polimi.it/miele/crosscompilers.tar.bz2
-tar xvjf crosscompilers.tar.bz2
-cd ..
-
 # Configure resp
 ./waf configure --with-systemc=External_tools/systemc-2.2.0 --with-tlm=External_tools/TLM2 --with-momh-header=/usr/local/include/libmomh
 ./waf
 
+# Cross-Compilers
+#     download
+wget home.dei.polimi.it/miele/cross-compilers.tar.bz2
+tar xvjf cross-compilers.tar.bz2
+cd ..
+
 # configure software
 cd software
-./waf configure --arm-compiler=../External_tools/crosscompilers/arm --sparc-compiler=../External_tools/crosscompilers/sparc
+./waf configure --arm-compiler=../External_tools/cross-compilers/arm --sparc-compiler=../External_tools/cross-compilers/sparc
 ./waf
 
 cd ..
+
+./waf -C
