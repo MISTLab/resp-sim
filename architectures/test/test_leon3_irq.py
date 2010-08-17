65ca36a2fe0b40b9464fe647e256dcc90dcccde0
jumpjoe78 <jumpjoe78@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2010-08-17 13:42:09 +0000

    Added an IRQ test example

---
+++ architectures/test/test_leon3_irq.py
@@ -0,0 +1,177 @@
+# -*- coding: iso-8859-1 -*-
+##############################################################################
+#
+#
+#         ___           ___           ___           ___
+#        /  /\         /  /\         /  /\         /  /\
+#       /  /::\       /  /:/_       /  /:/_       /  /::\
+#      /  /:/\:\     /  /:/ /\     /  /:/ /\     /  /:/\:\
+#     /  /:/~/:/    /  /:/ /:/_   /  /:/ /::\   /  /:/~/:/
+#    /__/:/ /:/___ /__/:/ /:/ /\ /__/:/ /:/\:\ /__/:/ /:/
+#    \  \:\/:::::/ \  \:\/:/ /:/ \  \:\/:/~/:/ \  \:\/:/
+#     \  \::/~~~~   \  \::/ /:/   \  \::/ /:/   \  \::/
+#      \  \:\        \  \:\/:/     \__\/ /:/     \  \:\
+#       \  \:\        \  \::/        /__/:/       \  \:\
+#        \__\/         \__\/         \__\/         \__\/
+#
+#
+#
+#
+#   This file is part of ReSP.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+#   (c) Giovanni Beltrame, Luca Fossati
+#       Giovanni.Beltrame@esa.int fossati@elet.polimi.it
+#
+#   Architecture written by: Fabio Arlati arlati.fabio@gmail.com
+#
+##############################################################################
+
+###### GENERAL PARAMETERS #####
+PROCESSOR_FREQUENCY = 1000        # MHz
+PROCESSOR_NUMBER  = 1             #
+try:
+    PROCESSOR_NAMESPACE
+except:
+    PROCESSOR_NAMESPACE = leon3_funcLT_wrapper.Processor_leon3_funclt
+
+# Memory/bus
+MEMORY_SIZE       = 32              # MBytes
+MEM_LATENCY       = 10.0            # ns
+BUS_ACTIVE        = True
+BUS_LATENCY       = 10.0            # ns
+
+# Software
+try:
+    SOFTWARE
+except:
+    SOFTWARE = 'irqTester'
+
+if SOFTWARE:
+    try:
+        ARGS
+    except:
+        ARGS = []
+
+OS_EMULATION = True     # True or False
+
+# Modified stats auto-printer
+def statsPrinter():
+    print '\x1b[34m\x1b[1mReal Elapsed Time (seconds):\x1b[0m'
+    print '\x1b[31m' + str(controller.print_real_time()) + '\x1b[0m'
+    print '\x1b[34m\x1b[1mSimulated Elapsed Time (nano-seconds):\x1b[0m'
+    print '\x1b[31m' + str(controller.get_simulated_time()) + '\x1b[0m'
+    if BUS_ACTIVE:
+        print '\x1b[34m\x1b[1mBus Accesses:\x1b[0m'
+        print '\x1b[31m' + str(bus.numAccesses) + '\x1b[0m'
+        print '\x1b[34m\x1b[1mBus Words:\x1b[0m'
+        print '\x1b[31m' + str(bus.numWords) + '\x1b[0m'
+
+################################################
+##### AUTO VARIABLE SETUP ######################
+################################################
+
+# Find the specified software in the _build_ directory if not an absolute path
+if not SOFTWARE or not os.path.isfile(SOFTWARE):
+    SOFTWARE = findInFolder(SOFTWARE, 'software/build/sparc/')
+    if not SOFTWARE:
+        raise Exception('Unable to find program')
+
+################################################
+##### COMPONENT CREATION #######################
+################################################
+
+###### PROCESSOR INSTANTIATION #####
+#Processor instantiation; decomment the following line to add a second processor and eventually add similar lines
+#for the third, fourth .... processor
+processors = []
+latency = scwrapper.sc_time(float(1000)/float(PROCESSOR_FREQUENCY),  scwrapper.SC_NS)
+for i in range(0, PROCESSOR_NUMBER):
+    processors.append(PROCESSOR_NAMESPACE('processor_' + str(i), latency))
+
+### IRQ Stuff
+irqGen = irqGenerator_wrapper.IrqGenerator("irqGen", scwrapper.sc_time(1000, scwrapper.SC_US));
+irqGen.initSocket.bind(processors[0].IRQ_port.socket);
+processors[0].irqAck.initSocket.bind(irqGen.targSocket);
+
+
+##### MEMORY INSTANTIATION #####
+memorySize = 1024*1024*MEMORY_SIZE
+latencyMem = scwrapper.sc_time(MEM_LATENCY, scwrapper.SC_NS)
+mem = MemoryLT32.MemoryLT32('mem', memorySize, latencyMem)
+
+################################################
+##### INTERCONNECTIONS #########################
+################################################
+
+if BUS_ACTIVE:
+    latencyBus = scwrapper.sc_time(BUS_LATENCY, scwrapper.SC_NS)
+    bus = BusLT32.BusLT32('bus',2*PROCESSOR_NUMBER,latencyBus)
+
+    ##### BUS CONNECTIONS #####
+    # Connecting the master components to the bus
+    for i in range(0, PROCESSOR_NUMBER):
+        connectPorts(processors[i], processors[i].instrMem.initSocket, bus, bus.targetSocket)
+        connectPorts(processors[i], processors[i].dataMem.initSocket, bus, bus.targetSocket)
+
+    connectPorts(bus, bus.initiatorSocket, mem, mem.targetSocket)
+
+    # Add memory mapping
+    bus.addBinding("mem",0x0,memorySize)
+else:
+    if PROCESSOR_NUMBER > 1:
+        raise Exception('Multi-core systems need to have an interconnection layer between processors and memory')
+    else:
+        ##### MEMORY CONNECTIONS #####
+        # Connecting the master component to the memory
+        raise Exception('Sorry, memory currently supports only a single connection (data or instruction). Please activate an interconnection layer between processors and memory')
+        #connectPorts(processors[0], processors[0].instrMem.initSocket, mem, mem.targetSocket)
+        #connectPorts(processors[0], processors[0].dataMem.initSocket, mem, mem.targetSocket)
+
+################################################
+##### SYSTEM INIT ##############################
+################################################
+
+##### LOAD SOFTWARE #####
+
+import os
+if not os.path.exists(SOFTWARE):
+    raise Exception('Error, ' + str(SOFTWARE) + ' does not exists')
+
+loader = loader_wrapper.Loader(SOFTWARE)
+#Initialization of the processors and loading in memory of the application program
+print "Writing memory"
+loader.loadProgInMemory(mem)
+
+print "Setting up CPU registers"
+for i in range(0, PROCESSOR_NUMBER):
+    processors[i].ENTRY_POINT = loader.getProgStart()
+    processors[i].PROGRAM_LIMIT = loader.getProgDim() + loader.getDataStart()
+    processors[i].PROGRAM_START = loader.getDataStart()
+    processors[i].resetOp()
+
+# Now I initialize the OS emulator
+print "Setting up OS Emulation"
+if OS_EMULATION:
+    trapwrapper.OSEmulatorBase.set_program_args(ARGS)
+    for i in range(0, PROCESSOR_NUMBER):
+        curEmu = trapwrapper.OSEmulator32(processors[i].getInterface())
+        curEmu.initSysCalls(SOFTWARE)
+        processors[i].toolManager.addTool(curEmu)
+
+# We can finally run the simulation
+# run_simulation()
