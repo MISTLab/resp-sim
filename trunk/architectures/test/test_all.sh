864eb5bfa8330608b82165fe285c20788bf58d57
pilato.christian <pilato.christian@a44dbb0e-540b-11de-8e0a-c97bf73df9f4>
2011-11-07 21:22:45 +0000

    Fixed regression script due to a missing architecture

--- trunk/architectures/test/test_all.sh
+++ trunk/architectures/test/test_all.sh
@@ -48,4 +48,3 @@
 ./startSim.sh -a architectures/test/test_coherent_caches.py --silent
 ./startSim.sh -a architectures/test/test_reconfig.py --silent
 ./startSim.sh -a architectures/test/test_reconfig_multi.py --silent
-./startSim.sh -a architectures/test/test_running_2_systems.py --silent
